c23c48bf3066dc0c9b694c70354dec3b41fcba10
amritkuikel <63916236+amritkuikel@users.noreply.github.com>
2023-11-13 17:50:34 +0545

    9

--- src/components/1-firstpage/2-pg1part2/2-videoportions/videoportions.jsx
+++ src/components/1-firstpage/2-pg1part2/2-videoportions/videoportions.jsx
@@ -1,20 +1,19 @@
 import ReactPlayer from "react-player";
-// import vid from "/public/assests/videos/exo.mp4";
 
 export default function VideoPortions() {
   return (
     <div className="bg-gray-400">
       <div className="text-black">fasdf</div>
-      {/* <ReactPlayer
-        url={vid}
+      <ReactPlayer
+        url='/assests/videos/exo.mp4'
         controls={false}
         playing={true}
         loop={true}
         muted={true}
         width="100%"
         height="100%"
-      /> */}
-      <video autoPlay loop  muted src="/assests/videos/exo.mp4" className="h-44 "/>
+      />
+      {/* <video autoPlay loop  muted src="/assests/videos/exo.mp4" className="h-44 "/> */}
     </div>
   );
 }
